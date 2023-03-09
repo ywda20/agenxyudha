78598f3a4363ee57f761d5cc9c81bb1d2a0501f5
Yudha Andrian <ywda20@gmail.com>
2023-03-09 16:14:32 +0700

    mengubah tabel penonton

--- database/migrations/2023_03_09_090713_penonton.php
+++ database/migrations/2023_03_09_090713_penonton.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('umur'); 
             $table->string('telp'); 
-            $table->timestamps("tanggalBeli");
+            $table->timestamps('tgl_beli');
         });
     }
 
